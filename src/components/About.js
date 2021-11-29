5cfadb0a7f65998f09268b3d8222f8b10ff55c04
deerslayer11pt <cwooldridge002@gmail.com>
2021-11-28 21:07:54 -0600

    name change

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,7 @@ const About = () => {
                     <div className="flex flex-col justify-center w-full px-8 mx-6 my-12 text-center rounded-md md:w-96 lg:w-80 xl:w-64 dark:bg-coolGray-100 dark:text-coolGray-800">
                         <img alt="" className="self-center w-24 h-24 -mt-12 bg-center bg-cover rounded-full dark:bg-coolGray-500" src="/genImgs/chris.png"/>
                         <div className="flex-1 my-4">
-                            <p className="text-xl font-semibold leading-snug text-siteColors-purp">??????</p>
+                            <p className="text-xl font-semibold leading-snug text-siteColors-purp">The Scientist</p>
                             <p className="text-siteColors-purp">Chief Game Designer</p>
                         </div>
                         <div className="flex items-center justify-center p-3 space-x-3 border-t-2 border-siteColors-purp">
